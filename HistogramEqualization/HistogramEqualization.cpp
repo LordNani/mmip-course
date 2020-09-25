54f765cf2bf6353ee629e9f0b5e51ed77a7e5704
LordNani <rigansu@gmail.com>
2020-09-25 19:45:46 +0300

    Histogram eq finished

--- HistogramEqualization/HistogramEqualization.cpp
+++ HistogramEqualization/HistogramEqualization.cpp
@@ -8,7 +8,9 @@
 using namespace cv;
 using namespace std;
 
-void HistogramEqualization(const Mat& in_image, Mat& out_image, Mat& in_histogram, Mat& out_histogram)
+const float INTESITY_MULTIPLIER = 255;
+
+void HistogramEqualization(const Mat& in_image, Mat& out_image)
 {
 	map<size_t, float> intensityMap;
 	float totalPixel = in_image.cols * in_image.rows;
@@ -37,16 +39,20 @@ void HistogramEqualization(const Mat& in_image, Mat& out_image, Mat& in_histogra
 	float cumulitiveValue = 0;
 	for (size_t i = 0; i < 256; i++)
 	{
-		
 		intensityMap[i] = intensityMap[i] / totalPixel + cumulitiveValue;
 		if (intensityMap[i] > 1)
 			intensityMap[i] = 1;
 
 		cumulitiveValue = intensityMap[i];
+		intensityMap[i] *= INTESITY_MULTIPLIER;
 		if (!NDEBUG) 
 			cout << "Pixel color " << i << "= CDF " << intensityMap[i] << "\n";
 	}
 
+	for (int i = 0; i < (int)totalPixel; i++)
+	{
+		out_image.data[i] = ((int)floor(intensityMap[in_image.data[i]])) % 256;
+	}
 
 }
 
@@ -65,6 +71,7 @@ Mat computeHistogram(Mat& input_image)
     minMaxLoc(histogram, 0, &max_val);
 
     cv::Mat3b hist_image = cv::Mat3b::zeros(256, 256);
+	normalize(histogram, histogram, 0, hist_image.rows, NORM_MINMAX, -1, Mat());
 
     // visualize each bin
     for (int b = 0; b < 256; b++) {
@@ -81,8 +88,8 @@ int main()
 {
     string imageNames[] = { "IM0.jpg", "IM1.tif", "IM_CAT.png","smallGradient.jpg", "IM10.tif", "IM8.tif", "IM23.tif", "IM17.tif", "IM13.tif", "IM11.tif" };
 
-    Mat img = imread("C:/Users/Lord/source/repos/Tif/" + imageNames[3], 0);
-    Mat newImg(img.rows, img.cols, CV_8UC1, Scalar(0));
+    Mat img = imread("C:/Users/Lord/source/repos/Tif/" + imageNames[2], 0);
+    Mat changedImg(img.rows, img.cols, CV_8UC1, Scalar(0));
 
     if (img.data == 0) // Check for invalid input
     {
@@ -95,8 +102,12 @@ int main()
 
     imshow("histogramBefore", computeHistogram(img));
     imshow("imageBefore", img);
-	HistogramEqualization(img, newImg, newImg, newImg);
-    //imshow(secondWindowName, newImg);
+
+	HistogramEqualization(img, changedImg);
+
+	imshow("histogramAfter", computeHistogram(changedImg));
+    imshow("imageAfter", changedImg);
+
     waitKey(0);
     system("pause");
     return 0;
