deff52e1efdf3d79fe350e85b8700c6858149099
LordNani <rigansu@gmail.com>
2020-10-06 10:51:19 +0300

    sobel

--- SobelOperator/SobelOperator.cpp
+++ SobelOperator/SobelOperator.cpp
@@ -10,15 +10,16 @@ using namespace std;
 
 const int ksizeSliderMax = 5;
 int ksizeSlider = 0;
-const int sigmaSliderMax = 20;
+const int sigmaSliderMax = 30;
 int sigmaSlider = 0;
-Mat img, changedImg, changedImg2;
+double sigmaValue = 0;
+Mat img, changedImg;
 
 void convolution(const Mat& in_image, const double* mask, int ksize, double koef, Mat& out_image)
 {
     double temp = 0;
     int hk = ksize / 2; // floor(ksize/2)
-    cout << "Hk = " << hk << endl;
+    cout << "KOEF = " << koef << endl;
     for (int i = hk; i < in_image.rows - hk; ++i) {
         for (int j = hk; j < in_image.cols - hk; ++j) {
             for (int ik = 0; ik < ksize; ++ik) {
@@ -28,7 +29,8 @@ void convolution(const Mat& in_image, const double* mask, int ksize, double koef
                 }
             }
             //cout <<"new value for pixel: " <<  (int)temp / koef << endl;
-            out_image.data[i * in_image.cols + j] = (int)round(temp * koef);
+			temp = abs(temp * koef) > 255 ? 255 : abs(temp * koef);
+            out_image.data[i * in_image.cols + j] = (int)temp;
             temp = 0;
         }
     }
@@ -45,7 +47,6 @@ void myGaussianBlur(const Mat& in_image, Mat& out_image, const int ksize, double
     for (int i = 0; i < ksize; ++i) {
         for (int j = 0; j < ksize; ++j) {
             dist = pow((i - hk), 2) + pow((j - hk), 2);
-            cout << dist << endl;
             mask[i * ksize + j] = exp(-1.0 * (dist / denumEuler)) / denumMain;
             sum += mask[i * ksize + j];
             //cout << mask[i * ksize + j] << "  ";
@@ -61,40 +62,57 @@ void myGaussianBlur(const Mat& in_image, Mat& out_image, const int ksize, double
     convolution(in_image, mask, ksize, sum, out_image);
 }
 
-void SobelOperator(const Mat& in_image, Mat& out_image) {
+void SobelOperator(const Mat& in_image, Mat& out_image)
+{
+    Mat blurredImg, verticalSobel, horizontalSobel;
+	blurredImg = in_image.clone();
+	verticalSobel = in_image.clone();
+	horizontalSobel = in_image.clone();
 
+    const double sigma = 0.73;
+    const double xMask[] =
+		{ -1, 0, 1,
+		-2, 0, 2,
+		-1, 0, 1 };
+	const double yMask[] =
+	{ -1, -2,-1,
+	0, 0, 0,
+	1, 2, 1 };
+	const double koef = 0.25;
+
+	myGaussianBlur(in_image, blurredImg, 5, sigmaValue);
+	//imshow("Gaussian", blurredImg);
+	convolution(blurredImg, xMask, 3, koef, horizontalSobel);
+	convolution(blurredImg, yMask, 3, koef, verticalSobel);
+	//imshow("Horizontal Sobel", horizontalSobel);
+	//imshow("Vertical Sobel", verticalSobel);
+	for (int i = 0; i < out_image.cols * out_image.rows; ++i) {
+		out_image.data[i] = sqrt(pow(verticalSobel.data[i], 2) + pow(horizontalSobel.data[i], 2));
+	}
 }
 
 static void on_trackbar_change(int, void* = 0)
 {
-    int ksizeVal = ((double)ksizeSlider / ksizeSliderMax) * 9;
-    double sigmaVal = ((double)sigmaSlider / sigmaSliderMax) * 5;
-    cout << "ksize = " << ksizeVal << ", sigma = " << sigmaVal << "\n";
-    if (ksizeVal % 2 == 0)
-        ksizeVal++;
-    myGaussianBlur(img, changedImg, ksizeVal, sigmaVal);
+    //int ksizeVal = ((double)ksizeSlider / ksizeSliderMax) * 9;
+	sigmaValue = ((double)sigmaSlider / sigmaSliderMax) * 2;
+    cout << "sigma" << sigmaValue << "\n";
+	imshow("Original", img);
 
-    ostringstream kernelSizeText;
-    kernelSizeText << "ksize = " << ksizeVal << ", sigma = " << sigmaVal;
-    putText(changedImg, kernelSizeText.str(), Point(20, 30),
-        FONT_HERSHEY_SIMPLEX, 0.9, 220, 2);
-
-    imshow("Gaussian blur", changedImg);
+	SobelOperator(img, changedImg);
+	imshow("Sobel", changedImg);
 }
 
-static void on_trackbar_change2(int, void* = 0)
-{
-    on_trackbar_change(ksizeSlider);
-}
+//static void on_trackbar_change2(int, void* = 0)
+//{
+//    on_trackbar_change(ksizeSlider);
+//}
 
 int main()
 {
-
     std::cout.sync_with_stdio(false);
-    std::string imageNames[] = { "Geneva.tif", "norway.jpg", "bigGradient.jpg", "portrait.jpg", "test.png", "anime.jpg", "IM23.tif", "IM17.tif", "IM13.tif", "IM11.tif" };
-    img = imread("C:/Users/Lord/source/repos/Tif/" + imageNames[0], 0);
+    std::string imageNames[] = { "Geneva.tif", "norway.jpg", "artem_1.jpg", "portrait.jpg", "test.png", "anime.jpg", "IM23.tif", "IM17.tif", "IM13.tif", "IM11.tif" };
+    img = imread("C:/Users/Lord/source/repos/Tif/" + imageNames[5], 0);
     changedImg = img.clone();
-    changedImg2 = img.clone();
 
     if (img.data == 0) // Check for invalid input
     {
@@ -104,29 +122,16 @@ int main()
 
     std::cout << "Original image dimensions : " << img.cols << 'x' << img.rows << std::endl;
 
-    //namedWindow("Gaussian blur", WINDOW_AUTOSIZE);
-    //resizeWindow("Gaussian blur", changedImg.cols, changedImg.rows);
+    namedWindow("Original", WINDOW_AUTOSIZE);
+    resizeWindow("Original", img.cols, img.rows);
     //createTrackbar("KernelSize", "Gaussian blur", &ksizeSlider, ksizeSliderMax, on_trackbar_change);
-    //createTrackbar("Sigma", "Gaussian blur", &sigmaSlider, sigmaSliderMax, on_trackbar_change2);
+    createTrackbar("Sigma", "Original", &sigmaSlider, sigmaSliderMax, on_trackbar_change);
 
     imshow("Original", img);
     //imshow("Gaussian blur", changedImg);
 
-    //double shrp_mask[] = { 1, 2, 1,
-    //    2, 4, 2,
-    //    1, 2, 1 };
-
-	//	double shrp_mask[] = { 0, -1, 0,
-	//- 1, 5, -1,
-	//0, -1, 0 };
-
-    //double koeff = 0;
-    //for (int i = 0; i < 9; ++i) {
-    //    koeff += shrp_mask[i];
-    //}
-
-    //convolution(img, shrp_mask, 3,1 / koeff, changedImg);
-    imshow("convolution", changedImg);
+	SobelOperator(img, changedImg);
+    imshow("Sobel", changedImg);
 
     waitKey(0);
     system("pause");
